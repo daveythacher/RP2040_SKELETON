57d31737df8713eb0f2cb360d3508af632d710ed
David Thacher <davidethacher@gmail.com>
2023-09-26 09:54:07 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y cmake git gcc-arm-none-eabi gcc g++ build-essential python3 doxygen graphviz
+sudo apt install -y cmake git gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib gcc g++ build-essential python3 doxygen graphviz
